a13555bad285eec639a6b6aad63fdafdfbe2109b
Ramūnas Jurgilas <ramunas.jurgilas@gmail.com>
2019-10-12 05:23:12 +0300

    Initial Commit

---
+++ currencies-xr/Event.swift
@@ -0,0 +1,41 @@
+//
+//  Event.swift
+//  currencies-xr
+//
+//  Created by Ramunas Jurgilas on 2019-10-12.
+//  Copyright © 2019 Ramūnas Jurgilas. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+extension Event {
+    static func create(in managedObjectContext: NSManagedObjectContext){
+        let newEvent = self.init(context: managedObjectContext)
+        newEvent.timestamp = Date()
+        
+        do {
+            try  managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }   
+}
+
+extension Collection where Element == Event, Index == Int {
+    func delete(at indices: IndexSet, from managedObjectContext: NSManagedObjectContext) {
+        indices.forEach { managedObjectContext.delete(self[$0]) }       
+ 
+        do {
+            try managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+}
